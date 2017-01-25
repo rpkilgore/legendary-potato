1317888056ba28ddca67c93e192e188e9de98acd
Robert Kilgore <rkilgore@columnit.com>
2017-01-25 17:20:55 -0500

    Code cleanup Ready for test!

--- manifests/helloworld.pp
+++ manifests/helloworld.pp
@@ -1,19 +1,19 @@
 class legendarypotato::helloworld (
-	$mhellomsg => $legendarypotato::params::mhellomsg,
-	$rootdir => $legendarypotato::params::rootdir,
-	$filename => $legendarypotato::params::filename,
+	$mhellomsg     => $legendarypotato::params::mhellomsg,
+	$rootdir       => $legendarypotato::params::rootdir,
+	$filename      => $legendarypotato::params::filename,
 	$contenforfile => $legendarypotato::params::contenforfile,
-	$createfile => $legendarypotato::params::createfile,
+	$createfile    => $legendarypotato::params::createfile,
 	) inherits $legendarypotato::params {
 	
-	notify{'mhellowmsg variable contains: $mhellomsg'} #just sends out a message with whatever was defined in the variable
+	notify{"hellowmsg variable contains: $mhellomsg"}
 	
-	if $createfile == 'true' {
+	if $createfile {
 		#then create a file
 		#the params folder will determine the location
 		#ie c:\ for windows or /etc/ for linux
 		file { 'fileexample':
-			path => "$rootdir/$filename",
+			path    => "$rootdir/$filename",
 			content => $contenforfile,
 		}
 	}
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,12 @@
 class legendarypotato::params {
 	
-	$mhellomsg => 'Hello World, Puppet has found you!',
-	$filename  => 'legendary-potato.txt',
+	$mhellomsg     => 'Hello World, Puppet has found you!',
+	$filename      => 'legendary-potato.txt',
 	$contenforfile => 'If you can read me then congrats! You opened a file! :)',
-	$createfile    => 'false',
+	$createfile    => false,
 	
-	case $os['family'] {
-		'debian': {
-			$rootdir => '/etc/',
-		}
-		'redhat': {
+	case $os['kernel'] {
+		'linux': {
 			$rootdir => '/etc/',
 		}
 		'windows': {
